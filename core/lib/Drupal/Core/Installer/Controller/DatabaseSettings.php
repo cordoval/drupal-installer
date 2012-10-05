a138cebb98d9390d78f5ce5785650393c00a347f
Claes Gyllensvärd <letharion@gmail.com>
2012-10-05 17:20:00 +0200

    Build the database settings form with drupal_build_form instead to make rendering of it work

--- core/lib/Drupal/Core/Installer/Controller/DatabaseSettings.php
+++ core/lib/Drupal/Core/Installer/Controller/DatabaseSettings.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * @file
+ * Handle the database settings.
+ */
 
 namespace Drupal\Core\Installer\Controller;
 
@@ -7,13 +11,26 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 /**
- * This is installer step 1.
+ * This is installer step 5.
  */
 class DatabaseSettings extends InstallController {
 
+  /**
+   * Display the database selection form to the user.
+   */
   public function interactive() {
     include_once DRUPAL_ROOT . '/core/includes/form.inc';
-    $form = drupal_get_form('install_settings_form', $this->state);
+
+    $install_state = $this->install_state;
+    $form_state = array(
+      // We need to pass $install_state by reference in order for forms to
+      // modify it, since the form API will use it in call_user_func_array(),
+      // which requires that referenced variables be passed explicitly.
+      'build_info' => array('args' => array(&$install_state)),
+      'no_redirect' => TRUE,
+    );
+    $form = drupal_build_form('install_settings_form', $form_state);
+
     return new Response(drupal_render($form));
   }
 }
